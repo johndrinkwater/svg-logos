6b9fb5f0bb9bf3721c7b03c05c91f1d8dca95abf
John Drinkwater <john@nextraweb.com>
2015-02-01 18:19:55 +0000

    This example page is complete
    The images included here are part of a future update to my personal
    website http://johndrinkwater.name/, they have been built in full from
    official sources (whether vector or bitmap) into SVG, and made to
    maintain their shape and colours, yet obviously have some trademark/
    copyright derivation, so I assign the SVG files as CC BY-SA for whatever
    value that means.

--- index.php
+++ index.php
@@ -1,29 +1,21 @@
 <!DOCTYPE html>
 <html>
 <style>
-
 body { color: black; background: #ddd; }
 img { padding: 1em; color: brown; }
 svg { width: 64px; height: 64px; }
 svg, svg path, svg .inverted { transition: color .35s, background .35s, fill .35s, stroke .35s; }
+svg { box-sizing: content-box; background: #999; }
+svg { border-radius: 50%; padding: 0.5em; }
 svg:hover { background: transparent; }
 svg:not(:hover) path[fill] { fill: #ddd; }
 svg:not(:hover) path[fill].inverted { fill: #999; stroke: #ddd; }
-svg { box-sizing: content-box; background: #999; }
-svg { border-radius: 50%; padding: 0.5em; }
 </style>
 <body>
 <?php
-
 foreach ( scandir( '.', 1) as $image ) {
 
-//	if ($image === '.' or $image === '..' or $image === '.git')
-//		continue;
 	if ( substr( $image, -3 , 3 ) === 'svg' ) {
-		// print '<img width="128" height="128" src="' . $image . '">';
 		print file_get_contents( $image ) ;
 	}
-
-// TODO: fix aspect
-// TODO: branding
 }
